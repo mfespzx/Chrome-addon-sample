097b3eed4ae5c0b8097e0337540e52d69a9ac34f
a24598 <atshsmt@gmail.com>
2024-05-10 03:31:50 +0900

    Add files via upload
    Google Chromeのアドオンとローカルサーバーを連携したURLリスト生成スクリプトのAddon側のコードです

---
+++ script.js
@@ -0,0 +1,50 @@
+let Data = {"Title": "", "URL": ""}
+chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+},tabs=>{
+    const tab = tabs[0]
+    Data.Title = tab.title;
+    Data.URL = tab.url;
+    console.log(`Title: ${Data.Title}`);
+    console.log(`URL: ${Data.URL}`);
+});
+window.addEventListener('load',()=>{
+    const txtBox = document.querySelector('input');
+	const get = document.getElementById('get');
+    document.querySelector('button.ttl').addEventListener('click',()=>{
+        txtBox.value = Data.Title;
+    });
+    document.querySelector('button.url').addEventListener('click',()=>{
+        txtBox.value = Data.URL;
+        get.href = "http://127.0.0.1/get/get.php?url="+Data.URL;
+    });
+    document.querySelector('button.bmark').addEventListener('click',()=>{
+        txtBox.value = `[${Data.Title}](${Data.URL})`;
+    });
+    document.querySelector('button.mov').addEventListener('click',()=>{
+    });
+
+    const cb = new ClipboardJS('button.copy');
+    const msgContainer = document.querySelector('div.msg');
+    const msgSuccess = document.querySelector('p.copied');
+    const msgFailed = document.querySelector('p.failed');
+    cb.on("success", function(e){
+        console.log('Copied Successfully.', e);
+        msgContainer.style.display = "block";
+        msgSuccess.style.display = "block";
+        setTimeout(()=>{
+            msgSuccess.style.display = "none";
+            msgContainer.style.display = "none";
+        },3000);
+    });
+    cb.on("error", function(e) {
+        console.error('Failed to Copy.', e);
+        msgContainer.style.display = "block";
+        msgFailed.style.display = "block";
+        setTimeout(()=>{
+            msgFailed.style.display = "none";
+            msgContainer.style.display = "none";
+        },3000);
+    });
+})
